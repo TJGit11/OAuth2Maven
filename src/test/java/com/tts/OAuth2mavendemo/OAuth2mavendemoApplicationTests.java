c5fd521fd493cf26420ce6471b8a3b1d5f3220aa
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-02-24 15:29:24 -0600

    oauth2 demo with maven

---
+++ src/test/java/com/tts/OAuth2mavendemo/OAuth2mavendemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.OAuth2mavendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2mavendemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
